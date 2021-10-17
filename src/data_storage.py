1c040fa68e82e5b7eb6d1bcb305909bef720c9bf
aberriel <anselmo.lira1@gmail.com>
2021-10-17 17:31:10 -0300

    Neste atualização, ajustei a forma como os dados dos usuários são guardados. Antes usava uma lista de dicionários explícita, que estava dando erro na criação do usuário. Neste caso, agora, é só uma lista simples.
    A correção feita gerou outro caso: agora não acesso mais o usuário pelo e-mail (pois antes
    considerava o "database" como um dicionário que tem o e-mail como chave). Então, passei a ter
    que buscar sempre o usuário (usando um next()).
    
    Então, tive que mexer no create e no update.

--- src/data_storage.py
+++ src/data_storage.py
@@ -1,27 +1,27 @@
 from user_model import UserModel
-from typing import List
 
 
 class DataStorage:
-    database = List[dict]
+    database = list()
 
     def __init__(self):
         pass
     
-    def create(self, first_name, last_name, email):
-        item_found = [x for x in self.database if x['email'] == email]
+    def create(self, first_name: str, last_name: str, email: str):
+        item_found = next((x for x in self.database if x['email'] == email), None)
         if item_found:
             raise Exception(f'Usuário já cadastrado com o e-mail {email}')
         user_data = UserModel(first_name, last_name, email)
-        self.database[user_data.email] = user_data.to_json()
+        self.database.append(user_data.to_json())
 
     def update(self, email, first_name, last_name):
-        if email not in self.database:
-            raise Exception(f'Usuário {email} não está cadastrado')
-        user_data_dict = self.database['email']
-        user_data = UserModel.from_dict(user_data_dict)
+        user_found = next((x for x in self.database if x['email'] == email), None)
+        if not user_found:
+            raise Exception(f'Usuário com {email} não está cadastrado')
+        user_data = UserModel.from_dict(user_found)
         user_data.update_name(first_name, last_name)
-        self.database['email'] = user_data.to_json()
+        self.database.remove(user_found)
+        self.database.append(user_data.to_json())
     
     def get(self, first_name, last_name):
         pass
